b715cdeef8703665c7766f7533017c83038f1967
Jonathan Culau <jonathan.culau@pucrs.br>
2021-01-22 09:00:36 -0300

    Adicionar arquivos de projeto.

---
+++ System/command.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO.Ports;
+
+namespace Sociedade_Serial
+{
+
+
+    
+    public struct Check
+    {
+        UInt16 size;
+        checkAlgorithm algorithm;
+        Byte offset;
+        UInt64 poly;
+        UInt64 initialValue;
+        UInt64 finalXOR;
+    }
+
+    public struct FLAG {
+        string name;
+        Boolean send;
+        UInt16 offset;
+        List <UInt16> newValue;
+    }
+
+
+    public enum checkAlgorithm
+    {
+        none,
+        checksum,
+        crc
+    }
+    class command
+    {
+        private string Name;
+        private List<UInt16> sendCommand;
+        private List<UInt16> expectAnswer;
+        private List<FLAG> SendFlags;
+        private List<FLAG> ReceiveFlags;
+
+
+        public Check check;
+        public command(string Name, string sendCommand, string expectAnswer, Check check)
+        {
+            this.Name = Name;
+            this.check = check;
+          
+        }
+        public List<UInt16> Send()
+        {
+            List<UInt16> send;
+            
+            
+            return send;
+           
+        }
+    }
+}
---
+++ User Controls/Test.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.IO.Ports;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System;
+namespace Sociedade_Serial
+{
+    /// <summary>
+    /// Interação lógica para Test.xam
+    /// </summary>
+    public partial class Test : UserControl
+    {
+
+        private SerialPort port;
+        private List<command> commands;
+
+        public Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
